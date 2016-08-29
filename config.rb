256fe22dc7d27ae524082da6392bcbc5228139ee
Maurizio De Magnis <root@olisti.co>
2016-08-29 18:17:21 +0200

    updates the base URL

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ end
 
 activate :dato,
   token: "c16eb24eb2470c8c879ea02fae64af62d6dd857b07b554899e",
-  base_url: "http://dato-biblioteca.desio.org"
+  base_url: "https://biblioteca.desio.org"
 
 configure :build do
   activate :minify_css
